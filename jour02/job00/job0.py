e8df6f2acd78cb7fb2b528d19a5d171ae4cbefd0
swattiez <swattiez@fr.scc.com>
2022-04-20 15:19:54 +0200

    update try

--- jour02/job00/job0.py
+++ jour02/job00/job0.py
@@ -5,9 +5,15 @@ class Personne:
     def __init__(self, nom , prenom):
         self.nom=nom
         self.prenom=prenom
+    def __str__(self):
+        return "Bonjour, je m'appelle {} {}".format(self.nom, self.prenom)
+    # def __repr__(self):
+    #     return "Bonjour, je m'appelle {} {}".format(self.nom, self.prenom)
     # def __del__(self): 
     #     print("je suis le destructeur")
     def SePresenter(nom, prenom):
+        # nom = ''
+        # prenom = ''
         print("Bonjour, je m'appelle {} {}".format(nom, prenom))
         p=Personne(nom='Wattiez', prenom='Sofiane') # Instanciation
         # p1=Personne('nom', 'prenom') # Instanciation
@@ -15,6 +21,6 @@ class Personne:
         return p
 if __name__ == '__main__':
     print(Personne.SePresenter())
-# p=SePresenter() # Appel de la fonction
+    # p=Personne.SePresenter() # Appel de la fonction
     p1=Personne.SePresenter() # Appel de la fonction
-    print(Personne.SePresenter(p1))
+    print(Personne.SePresenter( p1))
--- jour02/job01/job01.py
+++ jour02/job01/job01.py
@@ -7,14 +7,16 @@ class Livre:
         return "Le livre {} de {} a {} pages".format(self.titre, self.auteur, self.nbpages)
     def __repr__(self):
         return "Livre({}, {}, {})".format(self.titre, self.auteur, self.nbpages)
-class Autheur:
+
+class Auteur:
     def __init__(self, nom, prenom):
         self.nom = nom
         self.prenom = prenom
     def __str__(self):
         return "L'auteur {} {}".format(self.nom, self.prenom)
     def __repr__(self):
-        return "Autheur({}, {})".format(self.nom, self.prenom)
+        return "Auteur({}, {})".format(self.nom, self.prenom)
+        
 class Personne:
     def __init__(self, nom, prenom):
         self.nom = nom
