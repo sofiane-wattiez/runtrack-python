d8a1cae0ad0b96791fc4f968a6bb805879afdf9a
swattiez <swattiez@fr.scc.com>
2022-04-21 14:20:24 +0200

    update rectangle

--- jour01/job19/job19.py
+++ jour01/job19/job19.py
@@ -1,11 +1,14 @@
 L = int(input('Veuillez saisir le nombre de ligne : '))
 C = int(input('Veuillez saisir le nombre de colonnes : '))
-for i in range(1,L+1):
-    for j in range(1,C+1):
-        if i == 1 or i == L or j == 1 or j == C :
-            print("* ", end='')
-        else :
-            print("  ", end='')
+
+for i in range(1, L+1):
+    for j in range(1, C+1):
+        if i == 1 or i == L:
+            print("-", end='')
+        elif  j == 1 or j == C:
+            print("", end='|')
+        else:
+            print("", end=' ')
     print()
 
 # L = int(input('Veuillez saisir le nombre de ligne : '))
