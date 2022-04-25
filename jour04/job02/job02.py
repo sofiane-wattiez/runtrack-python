5617eccf4d15ced7630ce1233627e49ae8483cfc
swattiez <swattiez@fr.scc.com>
2022-04-25 11:32:07 +0200

    update

--- jour04/job02/job02.py
+++ jour04/job02/job02.py
@@ -1,4 +1,3 @@
-
 nombreEntier = int(input('Entrez un nombre entier pour renseignez la taille des mots que vous recherché : '))
 
 with open('data.txt', 'r') as f:
@@ -8,3 +7,6 @@ with open('data.txt', 'r') as f:
         for word in line:
             if len(word) == nombreEntier:
                 print(word)
+            for word in line:
+                if word.count(' ') == nombreEntier:
+                    print(word)
